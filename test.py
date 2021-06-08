1c72b83bcdd7cced9ff5334292ba48394f9e9a71
JackLin <jack070613@yahoo.com.tw>
2021-06-08 23:00:34 +0800

    add file

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # Deep Learning Clasee Week 2(Logistic Regression)
 
-import numpy as mp
+import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import scipy
--- test2.py
+++ test2.py
@@ -1,1 +1,21 @@
-print('I want to be happy!')
+%matplotlib inline
+import scipy
+import matplotlib
+import numpy as np
+from PIL import Image
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import h5py
+import time 
+
+
+
+
+my_image = 'shark.jpg'
+fname = './'+my_image
+image = np.array(plt.imread(fname))
+num_px = image.shape[1]
+
+
+plt.imshow(image)
+time.sleep(1000)
